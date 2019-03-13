ea291cee88318698f229c27436d788f1fae1c99b
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-03-13 12:54:29 +0300

    fix request

--- src/Request/V1/Save/User.php
+++ src/Request/V1/Save/User.php
@@ -2,7 +2,7 @@
 
 namespace Alexander1000\Clients\Users\Request\V1\Save;
 
-class User
+class User implements \JsonSerializable
 {
     /**
      * @var int|null
@@ -41,5 +41,42 @@ class User
         $this->login = $login;
         $this->emails = $emails;
         $this->phones = $phones;
+
+        if ($this->id !== null) {
+            if ($this->statusId === null) {
+                throw new \InvalidArgumentException('statusId is required');
+            }
+        }
+
+        if ($this->login === null && empty($this->emails) && empty($this->phones)) {
+            throw new \InvalidArgumentException('not defined identifier: login or phone or email');
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        $params = [];
+
+        if ($this->id !== null) {
+            $params = [
+                'id' => $this->id,
+                'statusId' => $this->statusId,
+            ];
+        }
+
+        $params['login'] = $this->login;
+
+        if (!empty($this->emails)) {
+            $params['emails'] = $this->emails;
+        }
+
+        if (!empty($this->phones)) {
+            $params['phones'] = $this->phones;
+        }
+
+        return $params;
     }
 }
